e399490c6caf8f3bf0da75882ea4b9d3a40cf14c
ku9nov <sergeyku9nov@gmail.com>
2025-07-11 11:42:47 +0300

    test: add tests for formatGreeting function

--- index.test.js
+++ index.test.js
@@ -1,7 +1,12 @@
-const helloWorld = require('./index');
+const { add, formatGreeting } = require('./index');
 
 test('helloWorld should print "Hello, world!"', () => {
   console.log = jest.fn(); 
   helloWorld();
   expect(console.log).toHaveBeenCalledWith("Hello, world!");
+});
+
+test('formatGreeting should return formatted greeting message', () => {
+  expect(formatGreeting('John')).toBe('Hello, John! Welcome to our application.');
+  expect(formatGreeting('Alice')).toBe('Hello, Alice! Welcome to our application.');
 });
