09b3c4561e126db4ebbec7628df8de31e0c593c8
ku9nov <sergeyku9nov@gmail.com>
2025-07-11 11:42:37 +0300

    feat: add formatGreeting utility function

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function helloWorld() {
     console.log("Hello, world!");
 }
-module.exports = helloWorld;
+
+/**
+ * Utility function to format a greeting message
+ * @param {string} name - The name to greet
+ * @returns {string} Formatted greeting message
+ */
+function formatGreeting(name) {
+  return `Hello, ${name}! Welcome to our application.`;
+}
+
+module.exports = {
+  add,
+  formatGreeting
+};
 helloWorld(); 
   
   
