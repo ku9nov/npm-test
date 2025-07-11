9ebd02ce312cdfefc15149cffd7f17f030bed9ff
ku9nov <sergeyku9nov@gmail.com>
2025-07-11 14:20:37 +0300

    feat(string): add string manipulation utilities

---
+++ utils.js
@@ -0,0 +1,45 @@
+/**
+ * String utility functions
+ */
+
+/**
+ * Capitalize the first letter of a string
+ * @param {string} str - The input string
+ * @returns {string} String with first letter capitalized
+ */
+function capitalize(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/**
+ * Reverse a string
+ * @param {string} str - The input string
+ * @returns {string} Reversed string
+ */
+function reverse(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  return str.split('').reverse().join('');
+}
+
+/**
+ * Count words in a string
+ * @param {string} str - The input string
+ * @returns {number} Number of words
+ */
+function wordCount(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  return str.trim().split(/\s+/).length;
+}
+
+module.exports = {
+  capitalize,
+  reverse,
+  wordCount,
+}; 
